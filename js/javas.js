cb3c914449ec3c414d1d4b00ba1771889f315fca
Agustina Lorusso Marin <agustinazoelm@gmail.com>
2022-08-10 21:56:49 -0300

    Entrega

--- js/javas.js
+++ js/javas.js
@@ -7,6 +7,14 @@ class Excursion{
         this.nroExcursion=nroExcursion;
     }
 }
+
+class Reserva{
+    constructor(numero,nombre,precioTotal){
+        this.numero=numero;
+        this.nombre=nombre;
+        this.precioTotal=precioTotal;
+    }
+}
 //Arreglo de excursiones dentro de un array
 const excursiones=[
     new Excursion("caminata por el perito moreno",1900,1),
@@ -14,40 +22,53 @@ const excursiones=[
     new Excursion("esqui por bulgaria",3900,3),
 ]
 
+
+//mensajes usados en el programa
 const msjeDatoNoValido="El dato ingresado no es valido.\n Intentelo nuevamente";
-const msjeIngresoCantPersonas="Ingrese cantidad de personas que asistiran a la actividad";
+const msjeIngCantPersonas="Ingrese cantidad de personas que asistiran a la actividad";
+const msjeContinuarCompra="Para añadir otra excursion ingrese SI, si quiere terminar ingrese NO";
+const msjeEliminarDeCarrito=' Quiere eliminar algo del carrito?\n Ingrese SI/NO:';
 
 //Funcion que muestra mensaje de valor total de excursion,actividades y cantidad de personas y luego el valor total de compra
 function menuPrincipal(){
     let continuar="si";
-    //array de excursiones disponibles
-    //const listaExcursiones=["caminata por el perito moreno","kiteboarding en italia","esqui por bulgaria"];
-    //Array de lista de excursiones q elije el usuario y lista de sus precios correspondientes
-    const excursionesElegidas=[];
-    const listaPrecios=[];
+    let checkout='';
+    const reservas=[];
+    let cont=1;
     while (continuar ==="si"){
-        const elegirExcursion=ingresoExcursion();
+        const datosExcursion=ingresoExcursion();
         const cantPersonas=ingresoCantPersonas();
-        const precio=calculoPrecio(elegirExcursion,cantPersonas);
-        const actividad=listaExcursiones[elegirExcursion-1];
-        excursionesElegidas.push(actividad);
-        listaPrecios.push(precio);
-        alert("Actividad: "+actividad+"\nCantidad de personas: "+ cantPersonas +"\nValor total de la excursion: "+ precio);
-        continuar=ingresoContinuar();
+        const precio=calculoPrecio(datosExcursion,cantPersonas);
+        //lista con las excursiones elegidas
+        reservas.push(new Reserva(cont,datosExcursion.nombre,precio));
+        let verReserva=`Actividad: ${datosExcursion.nombre}\nCantidad de personas: ${cantPersonas}\nValor total de la excursion: ${precio}`;
+        alert(verReserva);
+        continuar=validarIngreso(msjeContinuarCompra);
+        cont=cont+1;
     }
-    let total=valortotal(listaPrecios);
-    let carrito=eliminarDeCarrito(excursionesElegidas,total);
+    let eliminarDeCarrito='CHECKOUT\n';
+    for (const reserva of reservas){
+        checkout+=`${reserva.numero} - ${reserva.nombre} - $${reserva.precioTotal}\n`;
+    }
+    eliminarDeCarrito+= `${checkout} ${msjeEliminarDeCarrito}`;
+    let carrito=validarIngreso(eliminarDeCarrito);
     while (carrito==="si"){
-        const posicion=posicionAEliminar(excursionesElegidas);
-        //elimino de la lista de excursiones y lista de precios con la posicion.
-        excursionesElegidas.splice(posicion,1);
-        listaPrecios.splice(posicion,1);
-        total=valortotal(listaPrecios);
-        carrito=eliminarDeCarrito(excursionesElegidas,total);
+        let posicion=posicionAEliminar(checkout,reservas);
+        reservas.splice((posicion-1),1);
+        checkout='CHECKOUT\n';
+        for (const reserva of reservas){
+            checkout+=`${reserva.numero} - ${reserva.nombre} - $${reserva.precioTotal}\n`;
+        }
+        eliminarDeCarrito=`${checkout} ${msjeEliminarDeCarrito}`
+        carrito=validarIngreso(eliminarDeCarrito);
     }
-    alert("COMPRA FINAL\n"+"Excursiones:\n"+ excursionesElegidas.join("\n")+"\n"+"Valor total compra:" +total);
-
-
+    total=valorTotal(reservas);
+    checkout='COMPRA FINAL\n';
+        for (const reserva of reservas){
+            checkout+=`${reserva.numero} - ${reserva.nombre} - $${reserva.precioTotal}\n`;
+        }
+    checkout+=`Valor total compra:${total}`;
+    alert(checkout);
 }
 
 //Funcion que muestra el menu de excursiones,pide ingreso de excursion elegida y valida ese dato.
@@ -58,86 +79,63 @@ function ingresoExcursion(){
         menu +=`${excursion.nroExcursion} - ${excursion.nombre} - $${excursion.precio}\n`;
     }
     menu+="Ingrese el numero de la excursion elegida:";
-    let nroActividad= parseint(prompt(menu));
+    let nroActividad= parseInt(prompt(menu));
     //Busca si el numero esta en arreglo de excursiones.
-    let verificarNroExcursion= excursiones.find(excursion => excursion.nroExcursion === nroActividad);
-    while (verificarNroExcursion === undefined){
-        alert(msjeDatoNoValido)
-        nroActividad= parseint(prompt(menu));
+    let datosExcursion=undefined;
+     while (datosExcursion === undefined){
+        datosExcursion= excursiones.find(excursion => excursion.nroExcursion === nroActividad);
+        if (datosExcursion === undefined){ 
+            alert(msjeDatoNoValido);
+            nroActividad= parseInt(prompt(menu))};
     }
-    return(nroActividad);
+    return(datosExcursion);
 }
 
 //Funcion que pide la cantidad de personas a asistir a excursion y valida el dato.
 function ingresoCantPersonas(){
-    let cantidad=trim(prompt(msjeIngresoCantPersonas));
+    let cantidad=prompt(msjeIngCantPersonas);
+    cantidad=cantidad.trim();
     while (cantidad==""||isNaN(cantidad)|| cantidad<=0){
         alert(msjeDatoNoValido);
-        cantidad=prompt(msjeIngresoCantPersonas);
+        cantidad=prompt(msjeIngCantPersonas);
     }
     return(parseInt(cantidad));
 }
 
 //  Funcion que calcula el valor de cada excursion segun la cantidad de personas que asistiran
-function calculoPrecio(nroExcursion,numero){
-    let valor=0;
-    switch(nroExcursion){
-        case  1:
-            valor=excursion1["precio"]*numero;
-            break;
-        case 2:
-            valor=excursion2["precio"]*numero;
-            break;
-        case 3:
-            valor=excursion3["precio"]*numero;
-            break;
-    }
-    return(valor);
+function calculoPrecio(datos,numero){
+    return(datos.precio*numero);
 }
 
-//Funcion que se encargara de preguntar al usuario si va a seguir con otra compra  o quiere terminar 
-function ingresoContinuar(){
-    let continuar=prompt("Para añadir otra excursion ingrese SI, si quiere terminar ingrese NO");
-    continuar=continuar.toLowerCase();
-    while ((continuar!="si")&&(continuar!="no")){
-        alert("El dato ingresado no es valido, Ingrese nuevamente");
-        continuar=prompt("Para añadir otra excursion ingrese SI, si quiere terminar ingrese NO");
-        continuar=continuar.toLowerCase();
+//funcion que suma cada valor de las excursiones
+function valorTotal(reservas){
+    let valorTotal=0;
+    for (const reserva of reservas){
+        valorTotal=reserva.precioTotal+valorTotal
     }
-    return(continuar);
+    return(valorTotal);
 }
-//funcion que suma cada valor de la lista
-function valortotal(lista){
-    let total=0
-    for (let i= 0 ; i< lista.length;i++){
-        const valor=lista[i];
-        total=valor+total;
-    }
-    return(total)
-}
+
 //Funcion que pregunta si se quiere eliminar algo del carrito y verifica los datos.
-function eliminarDeCarrito(lista1,valor){
-    eliminar=prompt("Excursiones:\n"+ lista1.join("\n")+"\n"+"Valor total compra:" +valor+"\n"+"Quiere eliminar algo del carrito?\n"+"Ingrese SI/NO");
-    eliminar.toLocaleLowerCase();
-    while (eliminar!="si" && eliminar!="no"){
-        alert("El dato ingresado no es valido, Ingrese nuevamente");
-        eliminar=prompt("Excursiones:\n"+ lista1.join("\n")+"\n"+"Valor total compra:" +valor+"\n"+"Quiere eliminar algo del carrito?\n"+"Ingrese SI/NO");
+function validarIngreso(mensaje){
+    ingreso=prompt(mensaje);
+    ingreso=ingreso.toLocaleLowerCase();
+    while (ingreso!="si" && ingreso!="no"){
+        alert(msjeDatoNoValido);
+        ingreso=prompt(mensaje);
     } 
-    return(eliminar);
+    return(ingreso);
 }
+
 //Funcion que devuelve la posicion en la lista del objecto que se quiere eliminar
-function posicionAEliminar(lista1){
-    let eliminar=prompt("Ingrese nombre de excursion a eliminar:\n"+lista1.join("\n"));
-    eliminar.toLocaleLowerCase();
-    let posicion=lista1.indexOf(eliminar);
-    alert(posicion)
-    while (posicion===-1){
-        alert(posicion);
-        alert("El dato ingresado no corresponde a ninguna excursion\n Ingrese nuevamente")
-        eliminar=prompt("Ingrese nombre de excursion a eliminar:\n"+lista1.join("\n"));
-        posicion=lista1.indexOf(eliminar); 
+function posicionAEliminar(objetos,lista){
+    let posicion=parseInt(prompt(`Ingrese el numero de la excursion a eliminar:\n ${objetos}`));
+    let nroEliminar=lista.find( reserva => reserva.numero === posicion);
+    while (nroEliminar === undefined){
+        alert(msjeDatoNoValido);
+        posicion=parseInt(prompt(`Ingrese el numero de la excursion a eliminar:\n ${objetos}`));
     }
-    
+
     return(posicion);
 }
 
